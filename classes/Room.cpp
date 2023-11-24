ab8b33a6ffc29e3f71f1eade581e56af429b2117
Fahim Ahmed <fahim7711@gmail.com>
2023-11-24 23:59:33 +0000

    Refactored Room class

--- classes/Room.cpp
+++ classes/Room.cpp
@@ -2,15 +2,14 @@
 #include <string>
 using namespace std;
 
-// Room::Room(string, string, int, int, int, Item*, Enemy*, string*, bool) {
-
-// }
-
-// Room::~Room() {
-//     delete[] items;
-//     delete[] enemies;
-//     delete[] exits_;
-// }
+// Anytime a Room instance is created, we do not know items and enemies beforehand, as those
+// are created after rooms, and then assigned to rooms
+Room::Room(string id, string description, vector<string> exits, bool playerLocation = false) {
+    id_ = id;
+    description_ = description;
+    exits_ = exits;
+    player_location = playerLocation;
+}
 
 string Room::getId() const {
     return id_;
@@ -20,27 +19,22 @@ string Room::getDescription() const {
     return description_;
 }
 
-int Room::getItemAmount() const {
-    return item_amount;
-}
-
-int Room::getEnemyAmount() const {
-    return enemy_amount;
-}
-
-// This is used by gameflow for checking if exits requested by user do exits or not
+// This is used by gameflow for checking if exits requested by user do exits or not, 
+// and which ones
 string Room::getExits() const {
-    if (exits_) {
+    if (!exits_.empty()) {
         string exits;
 
         exits = "Your exits are: \n";
 
-        for (int i = 0; i < exit_amount; i++) {
-            exits += *(exits_+i); 
+        for (const string& exit: exits_) {
+            exits += exit + " \n";
         }
 
         return exits;
     }
+
+    return "No exits";
 }
 
 // This is used when user asks for possible exits
@@ -49,9 +43,9 @@ string Room::printExits() const {
 
     exits = "Your exits are: \n";
 
-    if (exits_) {
-        for (int i = 0; i < exit_amount; i++) {
-            exits += *(exits_+i); 
+    if (!exits_.empty()) {
+        for (const string& exit: exits_) {
+            exits += exit + " \n";
         }
     } else {
         exits += "There are no exits";
@@ -60,15 +54,28 @@ string Room::printExits() const {
     return exits;
 }
 
-
+// When user asks "go back", this will help find the previous room
 string Room::getPrevExit() const {
     return prev_exit;
 }
 
+// Function to be used in gameflow, add items one by one
+void Room::addItem(Item item) {
+    items_.push_back(item);
+} 
+
+// Function to be used in gameflow, add enemy/enemies one by one
+void Room::addEnemy(Enemy enemy) {
+    enemy_.push_back(enemy);
+}
+
+// This relies on the JSON map, where each room has a way to go back to previous room
+// from user's point of view
 void Room::setPrevExit(string exit) {
     prev_exit = exit;
 }
 
+// Set or unset the player location on rooms
 void Room::setPlayerLocation() {
     player_location = !player_location;
 }
--- classes/Room.h
+++ classes/Room.h
@@ -1,18 +1,16 @@
-// #include "Item.h"
-// #include "Enemy.h"
+#include "Item.h"
+#include "Enemy.h"
 #include <string>
+#include <vector>
 using namespace std;
 
 class Room {
 
 public:
-    // To delete
-    Room();
-
     // This constructor should define all items, enemies and exits
-    // Room(string, string, int, int, int, Item*, Enemy*, string*, string, bool);
+    Room(string, string, vector<string>, bool);
 
-    ~Room();
+    ~Room() = default;
 
     string getId() const;
 
@@ -20,42 +18,32 @@ public:
 
     int getItemAmount() const;
 
-    int getEnemyAmount() const;
-
     string getExits() const;
 
     string getPrevExit() const;
 
     // Player may take those items, and so they are no longer in the room
-    // Item setItem(string);
+    void addItem(Item);
 
     // Player may defeat those enemies, so there is no longer a threat in the room
-    // Enemy setEnemy(string);
+    void addEnemy(Enemy);
 
     void setPrevExit(string);
 
     void setPlayerLocation();
+
+    void setExits(vector<string> exits);
 
     string printExits() const;
 
 private:
     string id_;
     string description_;
-    int item_amount;
-    int enemy_amount;
-    int exit_amount;
-
-    // Dynamic array of Items
-    // Item* items;
-
-    // Dynamic array of Enemies
-    // Enemy* enemies;
-
-    // Dynamic array of exits
-    string* exits_;
-
     string prev_exit;
 
-    bool player_location;
+    vector<Item> items_;
+    vector<Enemy> enemy_;
+    vector<string> exits_;
 
+    bool player_location;
 };
